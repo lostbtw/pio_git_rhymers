c69dfbbad49b3a81eb8ead6aac1c15b4f370b954
253811 <253811@helium.com>
2025-03-24 11:29:41 +0100

    15. Walidacja projektu testami jednostkowymi

--- src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
+++ src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
@@ -4,7 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class RhymersJUnitTest {
-
+    //15. Projekt poprawny
     @Test
     public void testCountIn() {
         DefaultCountingOutRhymer rhymer = new DefaultCountingOutRhymer();
